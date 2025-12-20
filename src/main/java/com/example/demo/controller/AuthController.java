dfab22c84d25dfe0317535c34c92408f08284908
bala-amypo <bala@amypo.in>
2025-12-20 21:33:08 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
