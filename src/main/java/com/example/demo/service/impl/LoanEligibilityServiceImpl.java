966f89337fb898811b35b60520f6e4090411126c
bala-amypo <bala@amypo.in>
2025-12-20 21:13:20 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/LoanEligibilityServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/LoanEligibilityServiceImpl.java
@@ -3,21 +3,21 @@ package com.example.demo.service.impl;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;   
-import com.example.demo.entity.EligibityResult;
-import com.example.demo.repository.EligibityResultRepository;
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
 import org.springframework.web.bind.annotation.PathVariable;
-import com.example.demo.service.EligibityResultService;                
+import com.example.demo.service.LoanEligibilityService;                
 
 @Service
-public class EligibityResultServiceImpl implements EligibityResultService{
+public class LoanEligibilityServiceImpl implements LoanEligibilityService{
 
-    @Autowired EligibityResultRepository used;
+    @Autowired EliRepository used;
     @Override
-    public EligibityResult postData1(EligibityResult use){
+    public User postData1(User use){
         return used.save(use);  
     }
     @Override
-    public List<EligibityResult>getAllData1(){
+    public List<User>getAllData1(){
         return used.findAll();
     }
     @Override
@@ -26,11 +26,11 @@ public class EligibityResultServiceImpl implements EligibityResultService{
         return "Deleted successfully";
     }
     @Override
-    public EligibityResult getData1(Long id){
+    public User getData1(Long id){
     return used.findById(id).orElse(null);
     }
     @Override
-    public EligibityResult updateData1(Long id,EligibityResult entity){
+    public User updateData1(Long id,User entity){
         if(used.existsById(id)){
             entity.setId(id);
             return used.save(entity);
