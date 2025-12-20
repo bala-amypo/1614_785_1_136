9e20b1bf25310254f55133e0ab57286795ec2232
bala-amypo <bala@amypo.in>
2025-12-20 11:08:27 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/FinancialProfile.java
@@ -0,0 +1,22 @@
+package com.example.demo.entity;
+
+
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FinanciaProfile{
+    @Id
+    @GeneratedValue()
+    private Long id;
+    private String user;
+    private Double monthlyIncome;
+    private Double monthlyExpenses;
+    private Double existingLoanEmi;
+    private Integer creditScore;
+    private Double savingBalance;
+    private Timestamp lastUpdatedAt;
+
+}
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,21 +1,5 @@
 package com.example.demo.entity;
 
-
-
-
-@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class users{
-    
-    private Long id;
-    private String user;
-    private Double monthlyIncome;
-    private Double monthlyExpenses;
-    private Double existingLoanEmi;
-    private Integer creditScore;
-    private Double savingBalance;
-    private Timestamp lastUpdatedAt;
-
+public class User{
+    private Long
 }
