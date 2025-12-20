6e99323bdb35cc673949449d095c8280f6fa2e57
bala-amypo <bala@amypo.in>
2025-12-20 11:02:27 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,5 +1,13 @@
 package com.example.demo.entity;
 
 public class users{
-    private 
+    private Long id;
+    private String user;
+    private Double monthlyIncome;
+    private Double monthlyExpenses;
+    private Double existingLoanEmi;
+    private Integer creditScore;
+    private Double savingC;
+    private id long;
+
 }
