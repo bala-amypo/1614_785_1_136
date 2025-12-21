297bf837ed98a2d46f5cd2786e662fdcc1a16afb
bala-amypo <bala@amypo.in>
2025-12-21 11:50:44 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/FinancialProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/FinancialProfileServiceImpl.java
@@ -3,7 +3,7 @@ package com.example.demo.service.impl;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;   
-import com.example.demo.entity.EligibilityResult;
+import com.example.demo.entity.FinancialProfile;
 import com.example.demo.repository.EligibilityResultRepository;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.example.demo.service.FinancialProfileService;                
