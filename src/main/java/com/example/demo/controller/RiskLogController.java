ad0c447dc9dd797ec7dd8e1643c063f7ba828ffb
bala-amypo <bala@amypo.in>
2025-12-21 13:14:53 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/RiskLogController.java
@@ -0,0 +1,42 @@
+package com.example.demo.controller;
+
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.demo.entity.RiskAssessmentLog;
+// import com.example.demo.service.UserService;
+import com.example.demo.service.RiskService;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/User4")
+public class RiskLogController{
+    @Autowired  LoanRequestService ser;
+    @PostMapping("/register")
+    public LoanRequest sendData(@RequestBody LoanRequest stu){
+        return ser.postData4(stu);
+    }
+    @GetMapping("/get")
+    public List<LoanRequest> getval(){
+        return ser.getAllData4();
+    }
+    @DeleteMapping("/delete/{id}")
+    public String del(@PathVariable Long id){
+        return ser.DeleteData4(id);
+    }
+    @GetMapping("/find/{id}")
+    public LoanRequest find(@PathVariable Long id){
+        return ser.getData4(id);
+    }
+    @PutMapping("/put/{id}")
+    public LoanRequest putval(@PathVariable Long id,@RequestBody LoanRequest entity){
+        return ser.updateData4(id,entity);
+    }
+}
