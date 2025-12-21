096298ccc036e057e595bad36e0b9e13ff605b91
bala-amypo <bala@amypo.in>
2025-12-21 11:35:43 +0000

    User commit

--- src/main/java/com/example/demo/controller/FinancialProfileController.java
+++ src/main/java/com/example/demo/controller/FinancialProfileController.java
@@ -16,27 +16,27 @@ import com.example.demo.service.FinancialProfileService;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/User1")
+@RequestMapping("/User2")
 public class FinancialProfileController{
     @Autowired  FinancialProfileService ser;
     @PostMapping("/register")
     public FinancialProfile sendData(@RequestBody FinancialProfile stu){
-        return ser.postData2(stu);
+        return ser.postData3(stu);
     }
     @GetMapping("/get")
-    public List<EligibilityResult> getval(){
-        return ser.getAllData2();
+    public List<FinancialProfile> getval(){
+        return ser.getAllData3();
     }
     @DeleteMapping("/delete/{id}")
     public String del(@PathVariable Long id){
-        return ser.DeleteData2(id);
+        return ser.DeleteData3(id);
     }
     @GetMapping("/find/{id}")
-    public EligibilityResult find(@PathVariable Long id){
-        return ser.getData2(id);
+    public FinancialProfile find(@PathVariable Long id){
+        return ser.getData3(id);
     }
     @PutMapping("/put/{id}")
-    public EligibilityResult putval(@PathVariable Long id,@RequestBody EligibilityResult entity){
-        return ser.updateData2(id,entity);
+    public FinancialProfile putval(@PathVariable Long id,@RequestBody FinancialProfile entity){
+        return ser.updateData3(id,entity);
     }
 }
