b6c5335004a937eddbed3ea1f5024c6fd84eaa93
bala-amypo <bala@amypo.in>
2025-12-21 11:38:44 +0000

    User commit

--- src/main/java/com/example/demo/repository/FinancialProfileRepository.java
+++ src/main/java/com/example/demo/repository/FinancialProfileRepository.java
@@ -2,9 +2,9 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.example.demo.entity.EligibilityResult;
+import com.example.demo.entity.FinancialProfile;
 
 @Repository
-public interface EligibilityResultRepository extends JpaRepository<EligibilityResult,Long>{
+public interface FinancialProfileRepository extends JpaRepository<FinancialProfile,Long>{
 
 }
--- src/main/java/com/example/demo/service/FinancialProfileService.java
+++ src/main/java/com/example/demo/service/FinancialProfileService.java
@@ -1,4 +1,12 @@
-// public interface FinancialProfileService{
-//     FinancialProfile createOrUpdateProfile(FinancialProfile profile);
-//     FinancialProfile getProfileByUser(Long userId);
-// }
+package com.example.demo.service;
+
+import java.util.List;
+import com.example.demo.entity.FinancialProfile;
+
+public interface FinancialProfileService{
+    FinancialProfile postData2(FinancialProfile use1);
+    List<FinancialProfile>getAllData2();
+    String  DeleteData3(Long id);
+    FinancialProfile getData3(Long id);         
+    FinancialProfile updateData3(Long id,FinancialProfile entity);                                                        
+}
