42cbdf7aee6132aafc86666f277ee10f36b6c4e0
bala-amypo <bala@amypo.in>
2025-12-21 12:53:52 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/LoanRequestServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/LoanRequestServiceImpl.java
@@ -1,15 +1,40 @@
-// package com.example.demo.service.impl;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import com.example.demo.entity.StudentEntity;
-// import com.example.demo.service.StudentService;
-// import com.example.demo.repository.StudentRepository;
-// import org.springframework.stereotype.Service;
+package com.example.demo.service.impl;
 
-// @Service
-// public class LoanRequestServiceImpl implements UserService{
-//     @Autowired LoanRequestRepository student;
-//     @Override 
-//     public StudentEntity postData(StudentEntity stu){
-//         return student.save(stu);
-//     }
-// }
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;   
+import com.example.demo.entity.LoanRequest;
+import com.example.demo.repository.LoanRequestRepository;
+import org.springframework.web.bind.annotation.PathVariable;
+import com.example.demo.service.LoanRequestService;                
+
+@Service
+public class LoanEligibilityServiceImpl implements LoanEligibilityService{
+
+    @Autowired EligibilityResultRepository used;
+    @Override
+    public EligibilityResult postData2(EligibilityResult use){
+        return used.save(use);  
+    }
+    @Override
+    public List<EligibilityResult>getAllData2(){
+        return used.findAll();
+    }
+    @Override
+    public String DeleteData2(Long id){
+        used.deleteById(id);
+        return "Deleted successfully";
+    }
+    @Override
+    public EligibilityResult getData2(Long id){
+    return used.findById(id).orElse(null);
+    }
+    @Override
+    public EligibilityResult updateData2(Long id,EligibilityResult entity){
+        if(used.existsById(id)){
+            entity.setId(id);
+            return used.save(entity);
+        } 
+        return null;
+    }
+}
