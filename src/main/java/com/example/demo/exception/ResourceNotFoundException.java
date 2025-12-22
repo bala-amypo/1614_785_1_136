b93aa3b94abc2de1aaaba69cb566f27587949065
bala-amypo <bala@amypo.in>
2025-12-22 07:31:57 +0000

    User commit

--- src/main/java/com/example/demo/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/demo/exception/ResourceNotFoundException.java
@@ -1,1 +1,23 @@
-// fg
+package com.example.demo.exception;
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public static ResourceNotFoundException userNotFound() {
+        return new ResourceNotFoundException("User not found");
+    }
+
+    public static ResourceNotFoundException profileNotFound() {
+        return new ResourceNotFoundException("Financial profile not found");
+    }
+
+    public static ResourceNotFoundException requestNotFound() {
+        return new ResourceNotFoundException("Loan request not found");
+    }
+
+    public static ResourceNotFoundException resultNotFound() {
+        return new ResourceNotFoundException("Eligibility result not found");
+    }
+}
