5bb368e480976db098e2a51546a900510ab6605c
bala-amypo <bala@amypo.in>
2025-12-25 10:09:26 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -137,6 +137,57 @@
 
 
 
+// package com.example.demo.service.impl;
+
+// import com.example.demo.entity.User;
+// import com.example.demo.exception.BadRequestException;
+// import com.example.demo.exception.ResourceNotFoundException;
+// import com.example.demo.repository.UserRepository;
+
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+// import java.util.Optional;
+
+// public class UserServiceImpl {
+
+//     private final UserRepository userRepository;
+//     private final BCryptPasswordEncoder encoder;
+
+//     public UserServiceImpl(UserRepository userRepository) {
+//         this.userRepository = userRepository;
+//         this.encoder = new BCryptPasswordEncoder();
+//     }
+
+//     public User register(User user) {
+
+//         Optional<User> existing = userRepository.findByEmail(user.getEmail());
+//         if (existing.isPresent()) {
+//             throw new BadRequestException("Email already in use");
+//         }
+
+//         user.setPassword(encoder.encode(user.getPassword()));
+//         user.setRole(User.Role.CUSTOMER.name());
+
+//         return userRepository.save(user);
+//     }
+
+//     public User getById(Long id) {
+//         return userRepository.findById(id)
+//                 .orElseThrow(() ->
+//                         new ResourceNotFoundException("User not found"));
+//     }
+
+//     public User findByEmail(String email) {
+//         return userRepository.findByEmail(email).orElse(null);
+//     }
+// }
+
+
+
+
+
+
+
 package com.example.demo.service.impl;
 
 import com.example.demo.entity.User;
@@ -145,9 +196,11 @@ import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.UserRepository;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class UserServiceImpl {
 
     private final UserRepository userRepository;
