35dfa2bca239d9fbad8e0254809bf99066de9bde
bala-amypo <bala@amypo.in>
2025-12-25 09:54:27 +0000

    User commit

--- src/main/java/com/example/demo/repository/EligibilityResultRepository.java
+++ src/main/java/com/example/demo/repository/EligibilityResultRepository.java
@@ -12,14 +12,31 @@
 
 
 
+// package com.example.demo.repository;
+
+//  import org.springframework.data.jpa.repository.JpaRepository;
+//  import org.springframework.stereotype.Repository;
+//  import com.example.demo.entity.EligibilityResult;
+
+//  @Repository
+
+// public interface EligibilityResultRepository extends JpaRepository<EligibilityResult, Long> {
+//     Optional<EligibilityResult> findByLoanRequestId(Long id);
+// }
+
+
+
+
+
 package com.example.demo.repository;
 
- import org.springframework.data.jpa.repository.JpaRepository;
- import org.springframework.stereotype.Repository;
- import com.example.demo.entity.EligibilityResult;
+import com.example.demo.entity.EligibilityResult;
+import org.springframework.data.jpa.repository.JpaRepository;
 
- @Repository
+import java.util.Optional;   
 
-public interface EligibilityResultRepository extends JpaRepository<EligibilityResult, Long> {
-    Optional<EligibilityResult> findByLoanRequestId(Long id);
+public interface EligibilityResultRepository
+        extends JpaRepository<EligibilityResult, Long> {
+
+    Optional<EligibilityResult> findByLoanRequestId(Long loanRequestId);
 }
--- src/main/java/com/example/demo/repository/FinancialProfileRepository.java
+++ src/main/java/com/example/demo/repository/FinancialProfileRepository.java
@@ -11,13 +11,31 @@
 
 
 
- package com.example.demo.repository;
+//  package com.example.demo.repository;
 
- import org.springframework.data.jpa.repository.JpaRepository;
- import org.springframework.stereotype.Repository;
- import com.example.demo.entity.FinancialProfile;
+//  import org.springframework.data.jpa.repository.JpaRepository;
+//  import org.springframework.stereotype.Repository;
+//  import com.example.demo.entity.FinancialProfile;
 
- @Repository
-public interface FinancialProfileRepository extends JpaRepository<FinancialProfile, Long> {
+//  @Repository
+// public interface FinancialProfileRepository extends JpaRepository<FinancialProfile, Long> {
+//     Optional<FinancialProfile> findByUserId(Long userId);
+// }
+
+
+
+
+
+
+package com.example.demo.repository;
+
+import com.example.demo.entity.FinancialProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;   
+
+public interface FinancialProfileRepository
+        extends JpaRepository<FinancialProfile, Long> {
+
     Optional<FinancialProfile> findByUserId(Long userId);
 }
