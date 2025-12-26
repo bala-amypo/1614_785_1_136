dc971b7e1b8c35425dc8ce6509c64ed38c90758a
bala-amypo <bala@amypo.in>
2025-12-26 06:08:24 +0000

    User commit

--- src/main/java/com/example/demo/repository/RiskAssessmentLogRepository.java
+++ src/main/java/com/example/demo/repository/RiskAssessmentLogRepository.java
@@ -52,6 +52,5 @@ import java.util.Optional;
 
 public interface RiskAssessmentLogRepository
         extends JpaRepository<RiskAssessmentLog, Long> {
-
     Optional<RiskAssessmentLog> findByLoanRequestId(Long loanRequestId);
 }
