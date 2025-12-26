992709c34ce26414d3a687a1cfce11ee3cfe650a
bala-amypo <bala@amypo.in>
2025-12-26 06:11:24 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/RiskAssessmentService.java
@@ -0,0 +1,34 @@
+// package com.example.demo.service;
+
+// import com.example.demo.entity.RiskAssessmentLog;
+// import java.util.List;
+
+// public interface RiskAssessmentLogService {
+//     RiskAssessmentLog postData5(RiskAssessmentLog use);
+//     List<RiskAssessmentLog> getAllData5();
+//     String DeleteData5(Long id);
+//     RiskAssessmentLog getData5(Long id);
+//     RiskAssessmentLog updateData5(Long id, RiskAssessmentLog entity);
+// }
+
+
+
+
+
+package com.example.demo.service;
+
+import com.example.demo.entity.RiskAssessment;
+import java.util.List;
+
+public interface RiskAssessmentService {
+
+    RiskAssessment postData5(RiskAssessment use);
+
+    List<RiskAssessment> getAllData5();
+
+    String deleteData5(Long id);
+
+    RiskAssessment getData5(Long id);
+
+    RiskAssessment updateData5(Long id, RiskAssessment entity);
+}
