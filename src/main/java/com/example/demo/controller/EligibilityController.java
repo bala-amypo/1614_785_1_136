0315ce1717ef5dc3a1d5aa0283d93b066a716c3b
bala-amypo <bala@amypo.in>
2025-12-27 07:21:56 +0000

    User commit

--- src/main/java/com/example/demo/controller/EligibilityController.java
+++ src/main/java/com/example/demo/controller/EligibilityController.java
@@ -147,7 +147,49 @@
 
 
 
-// src/main/java/com/example/demo/controller/EligibilityController.java
+// // src/main/java/com/example/demo/controller/EligibilityController.java
+// package com.example.demo.controller;
+
+// import com.example.demo.entity.EligibilityResult;
+// import com.example.demo.service.EligibilityService;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.*;
+
+// @RestController
+// @RequestMapping("/eligibility")
+// public class EligibilityController {
+
+//     private final EligibilityService eligibilityService;
+
+//     public EligibilityController(EligibilityService eligibilityService) {
+//         this.eligibilityService = eligibilityService;
+//     }
+
+//     @PostMapping("/{loanRequestId}")
+//     public ResponseEntity<EligibilityResult> evaluate(@PathVariable Long loanRequestId) {
+//         EligibilityResult result = eligibilityService.evaluateEligibility(loanRequestId);
+//         return ResponseEntity.ok(result);
+//     }
+
+//     @GetMapping("/{loanRequestId}")
+//     public ResponseEntity<EligibilityResult> getByLoanRequest(@PathVariable Long loanRequestId) {
+//         EligibilityResult result = eligibilityService.getByLoanRequestId(loanRequestId);
+//         return ResponseEntity.ok(result);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
 package com.example.demo.controller;
 
 import com.example.demo.entity.EligibilityResult;
